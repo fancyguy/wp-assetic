1397685721d0c5ccfb6d63edfc1bcaf55514f30f
Steve Buzonas <steve@fancyguy.com>
2015-02-15 23:40:37 -0500

    initial commit

---
+++ plugin.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: FancyGuy Assetic
+Plugin URI: http://www.stevebuzonas.com/wordpress/plugins/assetic
+Author: Steve Buzonas
+Author URI: http://www.stevebuzonas.com
+License: MIT License
+License URI: http://sbuzonas.mit-license.org
+*/
